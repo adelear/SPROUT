df9da8da3d77505483436adef9c65aa53092f010
Rae Leda <veronica.rleda@gmail.com>
2024-07-05 09:03:32 -0400

    coins

--- Assets/Scripts/Mechanics/Score.cs
+++ Assets/Scripts/Mechanics/Score.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-    [SerializeField] int score = 10; 
+    [SerializeField] int score = 10;
+    [SerializeField] AudioClip collectSound; 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerController>())
         {
             GameManager.Instance.Score+=score;
+            AudioManager.Instance.PlayOneShotWithRandomPitch(collectSound, false, 0.8f, 1.2f);
             Destroy(gameObject); 
         }
     }
